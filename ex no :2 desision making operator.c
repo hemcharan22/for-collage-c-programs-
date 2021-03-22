0e16fd74e20aa3498476286b41c5da9325e7bccf
hemcharan <77770865+hemcharan22@users.noreply.github.com>
2021-03-22 20:42:00 +0530

    Create ex no :2 desision making operator.c

---
+++ ex no :2 desision making operator.c
@@ -0,0 +1,19 @@
+1.if statement
+2.switch statement
+3.conditional operator statement (? : operator)
+4.goto statement
+// choose any one
+
+1.simple if
+#include <stdio.h>
+
+void main( )
+{
+    int x, y;
+    x = 15;
+    y = 13;
+    if (x > y )
+    {
+        printf("x is greater than y");
+    }
+}
