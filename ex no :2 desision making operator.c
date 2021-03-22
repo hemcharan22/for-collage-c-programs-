230c755cd27f68f4d75b83be2d09973568c69e57
hemcharan <77770865+hemcharan22@users.noreply.github.com>
2021-03-22 20:54:40 +0530

    Update ex no :2 desision making operator.c

--- ex no :2 desision making operator.c
+++ ex no :2 desision making operator.c
@@ -38,6 +38,62 @@ void main( )
 
 //3.Nested if....else statement
 
+#include <stdio.h>
+void main( )
+{
+    int a, b, c;
+    printf("Enter 3 numbers...");
+    scanf("%d%d%d",&a, &b, &c);
+    if(a > b)
+    { 
+        if(a > c)
+        {
+            printf("a is the greatest");
+        }
+        else 
+        {
+            printf("c is the greatest");
+        }
+    }
+    else
+    {
+        if(b > c)
+        {
+            printf("b is the greatest");
+        }
+        else
+        {
+            printf("c is the greatest");
+        }
+    }
+}
+
+//4.else..if ladder
+
+#include <stdio.h>
+void main( )
+{
+    int a;
+    printf("Enter a number...");
+    scanf("%d", &a);
+    if(a%5 == 0 && a%8 == 0)
+    {
+        printf("Divisible by both 5 and 8");
+    }  
+    else if(a%8 == 0)
+    {
+        printf("Divisible by 8");
+    }
+    else if(a%5 == 0)
+    {
+        printf("Divisible by 5");
+    }
+    else 
+    {
+        printf("Divisible by none");
+    }
+}
+
 
 
 
