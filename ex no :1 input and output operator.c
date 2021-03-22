248db9c8c9643c1df8049568a49d4546f76ad1c6
hemcharan <77770865+hemcharan22@users.noreply.github.com>
2021-03-22 20:57:35 +0530

    Update ex no :1 input and output operator.c

--- ex no :1 input and output operator.c
+++ ex no :1 input and output operator.c
@@ -1,7 +1,33 @@
+//choose any one
+
+//1.first program
+
 #include <stdio.h>
 void main()
 {
     char line[30];
     gets (line);
     puts (line);
+}
+
+//2.second program
+
+#include<stdio.h>
+void main()
+{
+    // defining a variable
+    int i;
+    /* 
+        displaying message on the screen
+        asking the user to input a value
+    */
+    printf("Please enter a value...");
+    /*
+        reading the value entered by the user
+    */
+    scanf("%d", &i);
+    /*
+        displaying the number as output
+    */
+    printf( "\nYou entered: %d", i);
 }
