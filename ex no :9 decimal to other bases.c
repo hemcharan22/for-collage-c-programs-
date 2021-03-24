b6fdf163310f73708f3171f76bdbda295a18874d
hemcharan <77770865+hemcharan22@users.noreply.github.com>
2021-03-24 11:38:33 +0530

    Update ex no :9 decimal to other bases.c

--- ex no :9 decimal to other bases.c
+++ ex no :9 decimal to other bases.c
@@ -17,3 +17,16 @@ printf("%d",a[i]);
 }    
 return 0;  
 }  
+int convertDecimalToOctal(int decimalNumber)
+{
+    int octalNumber = 0, i = 1;
+
+    while (decimalNumber != 0)
+    {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
+        i *= 10;
+    }
+
+    return octalNumber;
+}
